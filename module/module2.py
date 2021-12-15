332d52c67141a84c6aa7bffeb08d08e2361c84ab
George <kesaev@gmail.com>
2021-12-16 00:22:11 +0200

    check 2 files

---
+++ module/module2.py
@@ -0,0 +1,19 @@
+def sum(
+    a: int,
+    b: int,
+) -> int:
+    return a + b
+
+
+def square(
+    a: int,
+    power: int = 1,
+) -> int:
+    return a ** power
+
+
+def multiply(
+    a: int,
+    b: int,
+) -> int:
+    return a * b
